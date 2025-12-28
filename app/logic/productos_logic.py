b8f9b87329a458d6ef1ac3f6e35e8761bb9a53c4
ANDRE <i2225436@continental.edu.pe>
2025-12-27 23:22:31 -0500

    se corrigio productos

--- app/logic/productos_logic.py
+++ app/logic/productos_logic.py
@@ -1,22 +1,18 @@
 from app.database.productos_db import insertar_producto, listar_productos, actualizar_producto
 
-def registrar_producto(nombre, descripcion, precio, stock, categoria_id):
+def registrar_producto(nombre, descripcion, precio, categoria_id):
     if not all([nombre, descripcion]):
         raise ValueError("No se permiten campos vacíos")
 
     if precio <= 0:
         raise ValueError("El precio debe ser mayor a 0")
 
-    if stock < 0:
-        raise ValueError("El stock no puede ser negativo")
 
     producto = {
         "nombre": nombre.strip(),
         "descripcion": descripcion.strip(),
         "precio": float(precio),
-        "stock": int(stock),
         "categoria_id": categoria_id,
-        "estado": True
     }
 
     return insertar_producto(producto)
@@ -24,7 +20,7 @@ def registrar_producto(nombre, descripcion, precio, stock, categoria_id):
 def obtener_productos():
     return listar_productos()
 
-def editar_producto(producto_id, nombre, descripcion, precio, stock, categoria_id, estado):
+def editar_producto(producto_id, nombre, descripcion, precio, categoria_id):
     if not producto_id:
         raise ValueError("Producto inválido")
 
@@ -32,9 +28,7 @@ def editar_producto(producto_id, nombre, descripcion, precio, stock, categoria_i
         "nombre": nombre,
         "descripcion": descripcion,
         "precio": float(precio),
-        "stock": int(stock),
         "categoria_id": categoria_id,
-        "estado": estado
     }
 
     return actualizar_producto(producto_id, datos)
